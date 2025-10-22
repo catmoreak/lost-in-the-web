30a4a0f70329237d4a15bf7040789e71f2b79c19
catmoreak <sujnanddevadiga@gmail.com>
2025-10-22 13:44:06 +0530

    [fix] Migrating to the EJS templatings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
-import { TRPCReactProvider } from "~/trpc/react";
+
 
 export const metadata: Metadata = {
   title: "Lost in the Web",
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+       {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import "./game/game.css";
+import "~/styles/game.css";
 
 export default function Home() {
   return (
